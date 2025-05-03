fdc024598e443ae7f82ccbc517060ad72217f3d8
gomezclaudio11 <claudiogomez23@gmail.com>
2025-05-02 22:38:30 -0300

    Reenderizado condicional

--- reactHelsinki/part1/src/App.jsx
+++ reactHelsinki/part1/src/App.jsx
@@ -1,46 +1,50 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return  (
-    <div>{counter}</div>
-  )
-}
-
-const Button = (props) => {
-  return(
-    <button onClick={props.onClick}>
-      {props.text}      
-    </button>
-  )
-}
-
-const App = () => {
-
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter) 
-    setCounter(counter + 1) 
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter -1)
-  }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  return (
-    <div>
+//reenderizado coondicional
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return(
       <div>
-      <Display counter = {counter}/>
+        THE APP IS USED BY PRESSING THE BUTTONS
       </div>
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
+    )
+  }
+  return(
+    <div>
+      button press history : {props.allClicks.join(" ")}
     </div>
   )
 }
 
+const Button = ({ handleClick, text }) => (  
+    <button onClick={handleClick}> 
+     {text}  
+    </button>
+  )
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L')) // La adición del nuevo elemento al array se logra con el método concat, que no muta el array existente, sino que devuelve una nueva copia del array con el elemento agregado.
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />      
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks= {allClicks}/>
+    </div>
+  )
+}
 export default App
