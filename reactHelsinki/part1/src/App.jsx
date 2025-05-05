1edb6fa48b823557adac58cd90be00549d4f0e8c
gomezclaudio11 <claudiogomez23@gmail.com>
2025-05-04 23:54:19 -0300

    Estadisticas

--- reactHelsinki/part1/src/App.jsx
+++ reactHelsinki/part1/src/App.jsx
@@ -1,27 +1,61 @@
 import { useState } from 'react'
 
-const Button = (props) =>(
-  <button onClick={props.handleClick}>
-    {props.text}
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
-const Display = props => <div> {props.value} </div>
+const StatisticsLine = ( {text, value}) => {
+  return(
+    <p> {text}: {value} </p>
+  )
+}
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const setToValue = (newValue) => {  
-      console.log('value now', newValue)  // imprime el nuevo valor en la consola    
-      setValue(newValue)  
+  const goodOpinion = () => {
+    setGood(good + 1)
   }
+
+  const neutralOpinion = () => {
+    setNeutral (neutral + 1)
+  }
+
+  const badOpinion = () => {
+    setBad (bad + 1)
+  }
+
+  const total = good + bad + neutral
+
+  const average = total === 0 ? 0 : (good - bad) / total
+
+  const positive = total === 0 ? 0 : (good / total) * 100
+
   return (
     <div>
-      <Display value = {value}/>
-      <Button handleClick = {() => setToValue(1000)} text = "thousand" />      
-      <Button handleClick = {() => setToValue(0)} text = "reset" />      
-      <Button handleClick = {() => setToValue(value + 1)} text = "increment" />      
-    </div>
+      <h1>give feedback</h1>
+      <Button handleClick={goodOpinion} text="good"/>
+      <Button handleClick={neutralOpinion} text="neutral"/>
+      <Button handleClick={badOpinion} text="bad"/>
+      <h2>statistics</h2>
+      { total === 0 ? (
+        <p> No feedback given</p>
+      ) : (
+      <div>
+      <StatisticsLine text="good" value = {good}/>
+      <StatisticsLine text="neutral" value = {neutral}/>
+      <StatisticsLine text="bad" value = {bad}/>
+      <StatisticsLine text="all" value = {total}/>
+      <StatisticsLine text="average" value = {average.toFixed(1)}/>
+      <StatisticsLine text="positive" value = {positive.toFixed(1) + "%"}/>
+      </div>
+    )}  
+  </div>
   )
 }
 
