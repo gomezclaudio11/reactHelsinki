88f5520b849de79d50b34a3d232c60b30f20462b
gomezclaudio11 <claudiogomez23@gmail.com>
2025-05-07 23:16:07 -0300

    Mapeo de un array / ejercicio de cursos

--- reactHelsinki/part1/src/components/Course.jsx
+++ reactHelsinki/part1/src/components/Course.jsx
@@ -1,24 +1,30 @@
-const Titulo = ({name}) => {
-  return (
-    <h1>{name}</h1>
-  )
+const Header = ({name}) => <h2>{name}</h2>
+
+const Part = ({part}) => {
+  <p>
+    {part.name} {part.exercises}
+  </p>
+}
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map(part =>(
+      <Part key={part.id} part={part}/>
+    ))}
+  </div>
+)
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <strong> total of {total} exercises</strong>
 }
 
 const Course = ({course}) =>{
-
     return (
       <div>        
-        <Titulo name={course.name }/>
-        {
-          course.parts.map(part => (
-            <p key={part.id}>
-              {part.name} {part.exercises}
-            </p>
-          ))
-        }
-        <p> total of {    
-        course.parts.reduce((sum, part) => sum + part.exercises, 0)
-        } excercises</p>
+        < Header  name={course.name}/> 
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>      
         </div>
     )
   }
