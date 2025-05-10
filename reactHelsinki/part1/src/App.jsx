7fc22393c7733e5976bef408d8f5965538ef527f
gomezclaudio11 <claudiogomez23@gmail.com>
2025-05-09 23:25:58 -0300

    Phonebook / useEffect / axios

--- reactHelsinki/part1/src/App.jsx
+++ reactHelsinki/part1/src/App.jsx
@@ -1,64 +1,78 @@
 import { useState, useEffect } from 'react'
-import Note from './components/Note'
+import Person from './components/Person'
 import axios from "axios"
 
   const App = () => {
-    const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState("")
-    const [showAll, setShowALL] = useState(true)
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState("")
 
     useEffect(() => {
       console.log("effect");
       axios
-        .get("http://localhost:3001/notes")
+        .get("http://localhost:3001/persons")
         .then(response => {
           console.log("PROMISE FULFILLED");
-          setNotes(response.data)
+          setPersons(response.data)
         })
     }, [])
-    console.log("render", notes.length, "notes");
     
-    const addNote = (event) => {    
-      event.preventDefault()    
-      const noteObject = {
-        content: newNote,
-        important: Math.random() > 0.5,
-        id: notes.length + 1
-      } 
-
-      setNotes(notes.concat(noteObject))
-      setNewNote("")
+    const addPerson = (event) =>{
+      event.preventDefault()
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+  
+      setPersons(persons.concat(personObject))
+      setNewName("")
+      setNewNumber("")
     }
-    
-    const handleNoteChange = (event) => {
-      console.log(event.target.value);
-      setNewNote(event.target.value)
-    }
-
-    const notesToShow = showAll ? notes : notes.filter (note => note.important)
-
-    return (
-      <div>
-        <h1>Notes</h1>
-      <div>
-      <button onClick={()=> setShowALL(!showAll)}>
-        show {showAll ? "important" : "all"}
-      </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) =>
-        <Note key={note.id} note ={note}/>
-          )
-        }
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote}
-              onChange={handleNoteChange}
-      />       
-      <button type="submit">save</button>      
-      </form>   
-      </div>
+  
+    const sameName = persons.some(
+      person => person.name.toLowerCase() === newName.toLowerCase()
     )
-  }
+    if(sameName){
+      alert (`${newName} ya existe`)
+      return
+    }
 
+    const nameToShow = persons.filter(person => 
+      person.name.toLowerCase().includes(filter.toLowerCase()) )
+      return (
+        <div>
+          <h2>Phonebook</h2>
+        <div>
+        filter shownn with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+        </div>
+        <ul>
+        {nameToShow.map(person => (
+          <Person key={person.id} person={person}/>
+          ))}
+        </ul>
+          <form onSubmit={addPerson}>
+          <div >
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+          </div>
+          <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
+          </div>
+          <div>
+          <button type="submit">add</button>
+          </div>
+          </form>
+          <h2>Numbers</h2>
+          <ul>
+          {
+            persons.map((person) => 
+              <Person Key={person.id} person={person} />
+            )
+            }
+          </ul>
+          </div>
+          )
+          }
+          
   export default App 
