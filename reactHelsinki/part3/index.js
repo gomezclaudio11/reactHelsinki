c9f6804efc0f0bfdfee89ad28ce0f4232a35ff15
gomezclaudio11 <claudiogomez23@gmail.com>
2025-05-27 00:25:20 -0300

    manejo de errores

--- reactHelsinki/part3/index.js
+++ reactHelsinki/part3/index.js
@@ -46,7 +46,7 @@ app.get("/api/notes/:id", (request, response, next) => {
         .catch (error => next(error))
 })
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
    const body = request.body
 
    if(!body.content) {
@@ -60,9 +60,11 @@ app.post("/api/notes", (request, response) => {
     important: Boolean(body.important) || false,
    })
 
-   note.save().then((savedNote) => {
+   note.save()
+   .then((savedNote) => {
     response.json(savedNote)
    })
+   .catch(error => next(error))
 })
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -74,14 +76,13 @@ app.delete("/api/notes/:id", (request, response, next) => {
 })
 
 app.put("api/notes/:id", (request, response, next) => {
-    const body = request.body
+    const {content, important} = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        {content, important},
+        {new: true, runValidators: true, context: "query"}
+        )
         .then(updatedNote => {
             response.json(updatedNote)
         })
