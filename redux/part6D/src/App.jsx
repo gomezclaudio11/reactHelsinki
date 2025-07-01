31a631b752bf829b9d3fe73110e95670e7e50aa3
gomezclaudio11 <claudiogomez23@gmail.com>
2025-06-30 23:29:41 -0300

    React query explicacion

--- redux/part6D/src/App.jsx
+++ redux/part6D/src/App.jsx
@@ -5,36 +5,39 @@ const App = () => {
   const queryClient = useQueryClient()
   
   const newNoteMutation = useMutation({ 
-    mutationFn: createNote,
-    onSuccess:(newNote) => {
+    mutationFn: createNote,//hace un POST con Axios.
+    onSuccess:(newNote) => { //que hace si el servidor devuelve ok
       const notes = queryClient.getQueriesData(["notes"])
+      // obtenés la cache actual con getQueriesData, y la actualizás localmente con la nueva nota.
       queryClient.setQueriesData(["notes"], notes.concat(newNote))
     }
   })
 
   const addNote = async (event) => {
     event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    newNoteMutation.mutate({ content, important: true})
+    const content = event.target.note.value //extrae texto de la nota
+    event.target.note.value = '' // limpia el input
+    newNoteMutation.mutate({ content, important: true}) //llama al mutate para hacer el POST
   }
-    const updateNoteMutation = useMutation({
-    mutationFn: updateNote,
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote, //hace un PUT con axios
     onSuccess: () => {
-      queryClient.invalidateQueries('notes')
+      queryClient.invalidateQueries('notes') //se invalida la query "notes" provocanndo un refresh
     },
   })
 
   const toggleImportance = (note) => {
     updateNoteMutation.mutate( {...note, important: !note.important })
-  }
-
+  } //Al hacer clic en un <li>, se invierte el valor de important y se dispara la mutación
+  
   const result = useQuery({    
     queryKey: ['notes'],    
     queryFn: getNotes,
-    refetchOnWindowFocus: false  
+    refetchOnWindowFocus: false  //evita que se vuelva a fetchear si el usuario cambia de pestaña y vuelve.
   })  
   console.log(JSON.parse(JSON.stringify(result)))
+
   if ( result.isLoading ) {    
     return <div>loading data...</div>  
   }
@@ -50,7 +53,7 @@ const App = () => {
       {notes.map(note =>
         <li key={note.id} onClick={() => toggleImportance(note)}>
           {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          <strong> {note.important ? 'important' : 'not important'}</strong>
         </li>
       )}
     </div>
@@ -82,4 +85,36 @@ una forma de almacenar temporalmente los datos servidos,
 sino también alguna solución para cómo se maneja el resto 
 del estado del frontend (por ejemplo, el estado de los 
 formularios o las notificaciones).
+ */
+/**
+ useQuery — para LEER datos
+Te permite hacer fetch (por ejemplo, un GET) y tener el 
+resultado + estados como isLoading, isError, etc. 
+
+ const { data, isLoading, isError } = useQuery({
+  queryKey: ['clave'],         // nombre único de la consulta
+  queryFn: () => fetchAlgo()   // la función que hace el GET
+})
+
+ */
+/**
+ useMutation — para CREAR, EDITAR o BORRAR datos
+Sirve para hacer acciones que cambian datos en el backend 
+(POST, PUT, DELETE), sin asociarse directamente a una 
+queryKey.
+ */
+/**
+ .mutate() — ejecuta la acción
+Usás .mutate() para disparar la mutación.
+Le pasás los datos que querés enviar al mutationFn.
+Cuando el backend responde bien, se ejecuta onSuccess.
+Si falla, se ejecuta onError.
+ */
+/**
+ onSuccess — lógica después de un OK
+Sirve para:
+    Actualizar cache
+    Navegar a otra página
+    Mostrar un mensaje
+    Invalidad queries
  */
--- redux/part6D/src/main.jsx
+++ redux/part6D/src/main.jsx
@@ -6,12 +6,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient() // instancia que maneja la cache, configuraciones, etc.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
+//un Provider que hace que toda tu app tenga acceso a React Query.
   <QueryClientProvider client={queryClient}>
     <App />
 
   </QueryClientProvider>
 )
+
+/**
+ React Query
+TanStack Query
+Es una librería que maneja la capa de datos asíncrona 
+(fetch, cache, sincronización y actualización de datos) 
+en apps React, de forma mucho más eficiente y simple que 
+usar solo useEffect + useState.
+ */
